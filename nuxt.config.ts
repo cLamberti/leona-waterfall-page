76c954030237c46ed8f6a7a47d4b9ca346214dc5
cLamberti <150824061+cLamberti@users.noreply.github.com>
2026-01-05 21:52:22 -0600

    Initialize Nuxt 4 project with starter files
    Set up a new Nuxt 4 project including configuration, basic app template, README, and .gitignore. Added Nuxt modules for icons, images, and fonts, and included initial public assets and TypeScript config.

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/fonts'
+  ],
+  
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
