5dc4d9c63fe4b02ccd2656097c64a8bc89fe0c20
Dereck Monge Chaves <168316345+Dmong04@users.noreply.github.com>
2026-01-08 19:28:41 -0600

    XD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['.app/assets/css/main.css'],
+  css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
   },
